e1f2e5c6e4546e655b62964927df38d7dd615f8e
ARMediaWorks <33051427+ARMediaWorks@users.noreply.github.com>
2017-10-25 17:08:29 +0900

    Add files via upload

---
+++ UnityTest/ARMediaWorks/ARMediaWorks/README.md
@@ -0,0 +1,2 @@
+# ARMediaWorks
+UnityTest
---
+++ UnityTest/ARMediaWorks/Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    int a=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (a == 0)
+        {
+            
+        }
+        else
+        {
+
+        }
+	}
+}
